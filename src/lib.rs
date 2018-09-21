67db4e83018b5515e2e393cc4ffefdc96c36be85
Terry Kerr <root@oefd.ca>
2018-09-21 18:54:40 -0400

    support asymmetric types for rx/tx

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+//! Bi-directional channels built on [crossbeam_channel](https://docs.rs/crossbeam-channel/0.2.6/crossbeam_channel/)
+//!
+//! # Examples
+//!
+//! ```rust
+//! # use doublecross::unbounded;
+//! # use std::thread;
+//! // doublecross supports different types for each
+//! // direction in the channel: the type arguments are
+//! // the receive/send types for the left channel, and 
+//! // the send/receive types for the right channel.
+//! let (left, right) = unbounded::<bool, i32>();
+//! thread::spawn(move || {
+//!     loop {
+//!         let val = right.recv().unwrap();
+//!         right.send(val % 2 == 0);
+//!     }
+//! });
+//!
+//! for i in 0..10 {
+//!     left.send(i);
+//!     if left.recv().unwrap() {
+//!         println!("{} is even", i);
+//!     }
+//! }
+//! ```
 #[macro_use]
 extern crate crossbeam_channel as channel;
 use channel::internal::select::{RecvArgument, SendArgument};
@@ -14,6 +40,13 @@ use std::option;
 /// its buffer is of length zero. Instead, send and receive operations
 /// must be executing at the same time in order to pair up and pass
 /// the message over.
+/// 
+/// # Type Arguments
+///
+/// Unforunately it is often necessary to annotate types to help the compiler;
+/// the type `T` refers to the type the left channel receives and the right
+/// channel sends, and the type `U` inversely refers to the type the right
+/// channel receives and the left channel sends.
 ///
 /// # Warning
 ///
@@ -27,7 +60,7 @@ use std::option;
 /// ```rust
 /// # use doublecross::bounded;
 /// # use std::thread;
-/// let (left, right) = bounded(0);
+/// let (left, right) = bounded::<(), ()>(0);
 ///
 /// thread::spawn(move || {
 ///     // ...
@@ -38,7 +71,7 @@ use std::option;
 /// right.recv().unwrap();
 /// println!("rendezvous complete");
 /// ```
-pub fn bounded<T>(cap: usize) -> (BiChannel<T>, BiChannel<T>) {
+pub fn bounded<T, U>(cap: usize) -> (BiChannel<T, U>, BiChannel<U, T>) {
     let (tx1, rx1) = channel::bounded(cap);
     let (tx2, rx2) = channel::bounded(cap);
     (BiChannel::new(tx1, rx2), BiChannel::new(tx2, rx1))
@@ -49,15 +82,22 @@ pub fn bounded<T>(cap: usize) -> (BiChannel<T>, BiChannel<T>) {
 /// This type of channel can hold any number of messages in
 /// either direction (ie: it has infinite capacity on both sides)
 ///
+/// # Type Arguments
+///
+/// Unforunately it is often necessary to annotate types to help the compiler;
+/// the type `T` refers to the type the left channel receives and the right
+/// channel sends, and the type `U` inversely refers to the type the right
+/// channel receives and the left channel sends.
+///
 /// # Examples
 ///
 /// ```rust
 /// # use doublecross::unbounded;
-/// let (left, right) = unbounded();
+/// let (left, right) = unbounded::<i32, i32>();
 /// left.send(10);
 /// assert_eq!(right.recv(), Some(10));
 /// ```
-pub fn unbounded<T>() -> (BiChannel<T>, BiChannel<T>) {
+pub fn unbounded<T, U>() -> (BiChannel<T, U>, BiChannel<U, T>) {
     let (tx1, rx1) = channel::unbounded();
     let (tx2, rx2) = channel::unbounded();
     (BiChannel::new(tx1, rx2), BiChannel::new(tx2, rx1))
@@ -65,17 +105,17 @@ pub fn unbounded<T>() -> (BiChannel<T>, BiChannel<T>) {
 
 /// Bi-directional communication build on, and
 /// usable with, crossbeam-channel channels.
-pub struct BiChannel<T> {
+pub struct BiChannel<T, U> {
     pub rx: Receiver<T>,
-    pub tx: Sender<T>,
+    pub tx: Sender<U>,
 }
 
-impl<T> BiChannel<T> {
-    pub fn new(tx: Sender<T>, rx: Receiver<T>) -> Self {
+impl<T, U> BiChannel<T, U> {
+    pub fn new(tx: Sender<U>, rx: Receiver<T>) -> Self {
         BiChannel { rx, tx }
     }
 
-    pub fn send(&self, msg: T) {
+    pub fn send(&self, msg: U) {
         self.tx.send(msg)
     }
 
@@ -84,7 +124,7 @@ impl<T> BiChannel<T> {
     }
 }
 
-impl<'a, T> RecvArgument<'a, T> for &'a BiChannel<T> {
+impl<'a, T, U> RecvArgument<'a, T> for &'a BiChannel<T, U> {
     type Iter = option::IntoIter<&'a Receiver<T>>;
 
     fn _as_recv_argument(&'a self) -> Self::Iter {
@@ -92,7 +132,7 @@ impl<'a, T> RecvArgument<'a, T> for &'a BiChannel<T> {
     }
 }
 
-impl<'a, T> SendArgument<'a, T> for &'a BiChannel<T> {
+impl<'a, T, U> SendArgument<'a, T> for &'a BiChannel<U, T> {
     type Iter = option::IntoIter<&'a Sender<T>>;
 
     fn _as_send_argument(&'a self) -> Self::Iter {
@@ -117,7 +157,7 @@ mod tests {
 
     #[test]
     fn rendezvous_recv_select() {
-        let (left, right) = super::bounded(0);
+        let (left, right) = super::bounded::<(), ()>(0);
         let timeout = Duration::from_millis(10);
 
         thread::spawn(move || {
@@ -134,7 +174,7 @@ mod tests {
 
     #[test]
     fn rendezvous_send_select() {
-        let (left, right) = super::bounded(0);
+        let (left, right) = super::bounded::<(), ()>(0);
         let timeout = Duration::from_millis(10);
 
         thread::spawn(move || {
@@ -147,5 +187,14 @@ mod tests {
                 panic!("timeout waiting for rendezvous");
             },
         }
+    }
+
+    #[test]
+    fn asymmetric_message_types() {
+        let (left, right) = super::unbounded::<u8, i16>();
+        left.send(0i16);
+        assert_eq!(right.recv().unwrap(), 0i16);
+        right.send(0u8);
+        assert_eq!(left.recv().unwrap(), 0u8);
     }
 }
